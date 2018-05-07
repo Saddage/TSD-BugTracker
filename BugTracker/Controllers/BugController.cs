1f9b7c002b0e5dd7874759bbe8c81b29eb265118
Milan Sawicki <milan.sawicki@rsqtechnologies.com>
2018-05-08 01:26:03 +0200

    crud support

--- BugTracker/Controllers/BugController.cs
+++ BugTracker/Controllers/BugController.cs
@@ -1,61 +1,93 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using BugTracker.Models;
+using MyWebApi.Models;
 using System.Collections.Generic;
 using System.Linq;
+using System;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using BugTracker.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace BugTracker.Controllers
 {
-    public class BugController : Controller
+
+	[Route("api/[controller]")]
+	public class BugController : ControllerBase
     {
-        private static readonly IList<Bug> _bugs;
+		private readonly MyWebApiContext _context;
 
-        static BugController()
+		public BugController(MyWebApiContext context)
+		{
+			_context = context;
+
+		}
+
+		[HttpGet]
+		public List<Bug> GetAll()
         {
-            _bugs = new List<Bug>
+			return _context.bugs.ToList();
+        }
+  
+		[HttpGet("{id}", Name = "GetTask")]
+        public IActionResult GetById(long id)
+        {
+			var item = _context.bugs.Find(id);
+            if (item == null)
             {
-                new Bug
-                {
-                    Id = 1,
-                    Title = "Bug buggowsky",
-                    Description = "Hello buggy bug!"
-                },
-                new Bug
-                {
-                    Id = 2,
-                    Title = "Bugg Bug bug",
-                    Description = "This is one bug"
-                },
-                new Bug
-                {
-                    Id = 3,
-                    Title = "bububugbu",
-                    Description = "This is *another* bug"
-                },
-            }; 
+                return NotFound();
+            }
+            return Ok(item);
         }
 
-        // GET: /<controller>/
-        public IActionResult Index()
+		[HttpDelete("{id}")]
+        public IActionResult Delete(long id)
         {
-            return View();
+			var bugs = _context.bugs.Find(id);
+            if (bugs == null)
+            {
+                return NotFound();
+            }
+
+			_context.bugs.Remove(bugs);
+            _context.SaveChanges();
+            return NoContent();
         }
 
-        [Route("bugs")]
-        [ResponseCache(Location = ResponseCacheLocation.None, NoStore = true)]
-        public ActionResult Comments()
+
+		[HttpPost]
+		public IActionResult Create([FromBody] Bug item)
         {
-            return Json(_bugs);
+            if (item == null)
+            {
+                return BadRequest();
+            }
+
+			_context.bugs.Add(item);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("GetTask", new { id = item.Id }, item);
         }
-		[Route("bug")]
-        [HttpPost]
-        public ActionResult AddComment(Bug bug)
+
+		[HttpPut("{id}")]
+		public IActionResult Update(long id, [FromBody] Bug item)
         {
-            _bugs.Add(bug);
-            return Content("Success :)");
+            if (item == null || item.Id != id)
+            {
+                return BadRequest();
+            }
+
+			var bugs = _context.bugs.Find(id);
+            if (bugs == null)
+            {
+                return NotFound();
+            }
+
+			bugs.Title = item.Title;
+			bugs.Description = item.Description;
+
+			_context.bugs.Update(bugs);
+            _context.SaveChanges();
+            return NoContent();
         }
     }
 }
